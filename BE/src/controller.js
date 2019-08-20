72df0e60176a30e3064ec1bb1231e406aeafc982
Nikita <mikita.savanovich@itechart-group.com>
2019-08-20 15:18:31 +0300

    add filters to get shows

--- BE/src/controller.js
+++ BE/src/controller.js
@@ -1,7 +1,15 @@
 const service = require('./service');
 
 const getShows = async (ctx) => {
-    const shows = await service.getShows();
+    const searchParams = {
+        title: ctx.query.title,
+        lowerThanRating: Number(ctx.query.max_rating),
+        higherThanRating: Number(ctx.query.min_rating),
+        releaseYear: Number(ctx.query.year),
+        categories: JSON.parse(ctx.query.categories),
+        limit: ctx.query.limit,
+    }
+    const shows = await service.getShows(searchParams);
 
     ctx.body = shows;
 }
--- BE/src/service.js
+++ BE/src/service.js
@@ -2,11 +2,59 @@ const fetch = require('node-fetch');
 
 const SHOWS_URL = 'http://54.172.194.177/tv.json';
 
-const getShows = async () => {
+const getShows = async ({
+    title,
+    lowerThanRating,
+    higherThanRating,
+    releaseYear,
+    categories
+}) => {
     const response = await fetch(SHOWS_URL);
     const json = await response.json();
 
-    return json;
+    const filteredShows = json.filter((show) => {
+        const filterResults = [];
+
+        if (title) {
+            filterResults.push(
+                show.title.toLowerCase().includes(title.toLowerCase())
+            );
+        }
+
+        const showRating = show.imdb
+            ? Number(show.imdb.split('/')[0])
+            : null;
+
+        if (!showRating && (lowerThanRating || higherThanRating)) {
+            filterResults.push(false);
+        } else if (showRating) {
+            lowerThanRating = lowerThanRating || 10;
+            higherThanRating = higherThanRating || 0;
+
+            filterResults.push(
+                showRating
+                    ? showRating >= higherThanRating
+                        && showRating <= lowerThanRating
+                    : false
+            );
+        }
+
+        if (releaseYear) {
+            const showReleaseYear = new Date(show.date_released).getFullYear();
+            filterResults.push(showReleaseYear === releaseYear);
+        }
+
+        if (categories) {
+            const showCategories = show.category.split('\n').map((category) => category.trim());
+
+            const passed = categories.every((category) => showCategories.includes(category));
+            filterResults.push(passed);
+        }
+
+        return filterResults.every((result) => !!result);
+    });
+
+    return filteredShows;
 }
 
 module.exports = {
