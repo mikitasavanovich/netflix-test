dbc62bab9cf6fd73e544d19d36ca032fb2399ea6
Nikita <mikita.savanovich@itechart-group.com>
2019-08-21 10:48:22 +0300

    add error check and cors for server

--- BE/src/index.js
+++ BE/src/index.js
@@ -1,10 +1,20 @@
 const Koa = require('koa');
+const cors = require('@koa/cors');
 const router = require('./router');
 
 const PORT = process.env.PORT || 3000;
 
 const app = new Koa();
 
+app.use(cors());
+app.use(async (ctx, next) => {
+    try {
+        await next();
+    } catch (error) {
+        console.error(error);
+        ctx.status = 500;
+    }
+})
 app.use(router.routes());
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
