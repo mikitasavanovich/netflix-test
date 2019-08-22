c78a99dea3606e3264fa0364291de5e262a2227b
Nikita <mikita.savanovich@itechart-group.com>
2019-08-22 15:09:36 +0300

    add badges for selected categories

--- FE/src/components/CategoryInput/CategoryInput.js
+++ FE/src/components/CategoryInput/CategoryInput.js
@@ -3,14 +3,26 @@ import { getCategories } from '../../api';
 
 import './CategoryInput.css';
 
-const CategoryInput = () => {
+const CategoryInput = ({ selectedCategories = [], onChange }) => {
     const [categories, setCategories] = useState([]);
     const [filteredCategories, setFilteredCategories] = useState([]);
     const [inputCategory, setInputCategory] = useState('');
 
-    const handleInputChange = ({ target: { value }}) => (
+    const handleInputChange = ({ target: { value } }) => (
         setInputCategory(value.toLowerCase())
     )
+
+    const selectCategory = ({ target: { innerText: category } }) => {
+        const newCategories = [...selectedCategories, category];
+        onChange('categories', newCategories);
+        setInputCategory('');
+    }
+
+    const removeCategory = ({ target }) => {
+        const removedCategory = target.dataset.category;
+        const newCategories = selectedCategories.filter((category) => category !== removedCategory);
+        onChange('categories', newCategories);
+    }
 
     useEffect(() => {
         const fetchCategories = async () => {
@@ -29,20 +41,53 @@ const CategoryInput = () => {
 
         const filteredCategories = categories.filter((category) => (
             category.toLowerCase().includes(inputCategory)
+                && !selectedCategories.includes(category)
         ));
         setFilteredCategories(filteredCategories);
     }, [inputCategory]);
 
     return (
-        <div className='form-group category-input'>
-            <label className='text-white' htmlFor='category'>Categories</label>
-            <input type='text' className='form-control' id='category' name='category' onChange={handleInputChange} />
-            <div className='list-group category-hint'>
-                {filteredCategories.map((category) => (
-                    <span className='list-group-item list-group-item-action'>{category}</span>
+        <>
+            <div className='category-input'>
+                {selectedCategories.map((category) => (
+                    <span
+                        key={`selected-category-${category}`}
+                        className='badge badge-light'
+                    >
+                        {category}
+                        <span 
+                            className='badge-close' 
+                            data-category={category}
+                            onClick={removeCategory}
+                        >
+                            x
+                        </span>
+                    </span>
                 ))}
+                <div className='form-group'>
+                    <label className='text-white' htmlFor='category'>Categories</label>
+                    <input
+                        type='text'
+                        className='form-control'
+                        id='category'
+                        name='category'
+                        value={inputCategory}
+                        onChange={handleInputChange}
+                    />
+                    <div className='list-group category-hint'>
+                        {filteredCategories.map((category) => (
+                            <span
+                                key={`category-hint-${category}`}
+                                className='list-group-item list-group-item-action'
+                                onClick={selectCategory}
+                            >
+                                {category}
+                            </span>
+                        ))}
+                    </div>
+                </div>
             </div>
-        </div>
+        </>
     );
 }
 
--- FE/src/components/SearchBar/SearchBar.js
+++ FE/src/components/SearchBar/SearchBar.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import CategoryInput from '../CategoryInput';
 
+import './SearchBar.css';
+
 const SearchBar = ({ searchForShows, setSearchValues, searchValues }) => {
-    const changeFormValue = ({ target: input }) => {
+    const changeFormValue = (name, value) => {
         setSearchValues({
             ...searchValues,
-            [input.getAttribute('name')]: input.value
+            [name]: value
         });
+    }
+
+    const handleChange = ({ target: input }) => {
+        changeFormValue(input.getAttribute('name'), input.value);
     }
 
     const submitForm = (event) => {
@@ -16,24 +22,26 @@ const SearchBar = ({ searchForShows, setSearchValues, searchValues }) => {
 
     return (
         <form className='search-bar' onSubmit={submitForm}>
-            <div className='search-bar__inputs d-flex justify-content-between'>
+            <div className='search-bar__inputs search-bar__inputs--upper d-flex justify-content-between'>
                 <div className='form-group'>
                     <label className='text-white' htmlFor='title'>Title</label>
-                    <input type='text' className='form-control' id='title' name='title' onChange={changeFormValue} />
+                    <input type='text' className='form-control' id='title' name='title' onChange={handleChange} />
                 </div>
                 <div className='form-group'>
                     <label className='text-white' htmlFor='rating-min'>Rating from</label>
-                    <input type='text' className='form-control' id='rating-min' name='minRating' onChange={changeFormValue} />
+                    <input type='text' className='form-control' id='rating-min' name='minRating' onChange={handleChange} />
                 </div>
                 <div className='form-group'>
                     <label className='text-white' htmlFor='rating-max'>Rating to</label>
-                    <input type='text' className='form-control' id='rating-max' name='maxRating' onChange={changeFormValue} />
+                    <input type='text' className='form-control' id='rating-max' name='maxRating' onChange={handleChange} />
                 </div>
+            </div>
+            <div className='search-bar__inputs search-bar__inputs--lower d-flex justify-content-between'>
                 <div className='form-group'>
                     <label className='text-white' htmlFor='release-year'>Release year</label>
-                    <input type='text' className='form-control' id='release-year' name='releaseYear' onChange={changeFormValue} />
+                    <input type='text' className='form-control' id='release-year' name='releaseYear' onChange={handleChange} />
                 </div>
-                <CategoryInput />
+                <CategoryInput onChange={changeFormValue} selectedCategories={searchValues.categories} />
             </div>
             <button type='submit' className='btn btn-light'>Apply filters</button>
         </form>
