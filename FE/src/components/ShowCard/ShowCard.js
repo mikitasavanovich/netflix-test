c4a29053b5546aa23f38fbf4f330ea8910b87625
Nikita <mikita.savanovich@itechart-group.com>
2019-08-22 15:55:44 +0300

    add categories to show cards

--- FE/src/components/ShowCard/ShowCard.js
+++ FE/src/components/ShowCard/ShowCard.js
@@ -4,15 +4,23 @@ const ShowCard = ({
     show: {
         title,
         imdb,
-        date_released
+        date_released,
+        category
     }
 }) => {
+    const categories = category.split('\n').map((item) => item.trim());
+
     return (
         <div className='card'>
             <div className='card-body'>
                 <h5 className='card-title'>{title}</h5>
                 <p className='card-text'>Imdb: {imdb || '-'}</p>
                 <p className='card-text'>Release date {new Date(date_released).toLocaleDateString()}</p>
+                <p className='card-text'>
+                    {categories.map((item) => (
+                        <span className='badge badge-secondary mx-1'>{item}</span>
+                    ))}
+                </p>
             </div>
         </div>
     );
